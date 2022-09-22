1300319d08613b30ccebcb180e15e6499d827bf0
Rasic2 <1051987201@qq.com>
2022-09-22 22:24:54 +0800

    format code

--- transform.sh
+++ transform.sh
@@ -8,60 +8,58 @@ RESET="\e[0m"
 workdir=
 input_format="msi"
 output_format=
-while [ $# -gt 0 ]
-do
-    if [[ $1 =~ "-i" ]];then
-        if [ x"$2" != x ];then
-            input_format=$2
-        else
-            echo -e "$RED Please specify a input_format $RESET"
-            exit 1
-        fi
-        shift
-        shift 
-    elif [[ $1 =~ "-o" ]];then 
-            if [ x"$2" != x ];then
-                output_format=$2
-        else
-            echo -e "$RED Please specify a output_format $RESET"
-            exit 2
-        fi
-        shift
-        shift 
+while [ $# -gt 0 ]; do
+  if [[ $1 =~ "-i" ]]; then
+    if [ x"$2" != x ]; then
+      input_format=$2
     else
-        workdir=$1
-        shift
+      echo -e "$RED Please specify a input_format $RESET"
+      exit 1
     fi
+    shift
+    shift
+  elif [[ $1 =~ "-o" ]]; then
+    if [ x"$2" != x ]; then
+      output_format=$2
+    else
+      echo -e "$RED Please specify a output_format $RESET"
+      exit 2
+    fi
+    shift
+    shift
+  else
+    workdir=$1
+    shift
+  fi
 done
 
-if [ x"$output_format" == x ];then
-    echo -e "$RED Please specify a output_format $RESET"
-    exit 2
+if [ x"$output_format" == x ]; then
+  echo -e "$RED Please specify a output_format $RESET"
+  exit 2
 fi
 
-if [ x"$workdir" == x ];then
-    echo -e "$RED Please specify a workdir $RESET"
-    exit 3
+if [ x"$workdir" == x ]; then
+  echo -e "$RED Please specify a workdir $RESET"
+  exit 3
 fi
 
 # print infomation
 echo -e "--> The workdir is $RED$workdir$RESET, input_format is $GREEN$input_format$RESET, output_format is $YELLOW$output_format$RESET <--"
 
-# transform file formats 
-AllFiles=`find $workdir -iname "*.$input_format"`
-for file in $AllFiles
-do
-    prefix=${file%.*}
-    if [ $output_format == "POSCAR" ];then
-        parent=`dirname $file`
-        name=`basename $file`
-        prefix=${name%.*} 
-        output="$parent/$output_format"_"$prefix"
-        
-        echo "$file -> $output"
-        `obabel $file -oPOSCAR -O $output 2>/dev/null` 
-    else
-        echo "$file -> $prefix.$output_format"
-        `obabel $file -O $prefix.$output_format 2>/dev/null`    
-    fi
+# transform file formats
+AllFiles=$(find $workdir -iname "*.$input_format")
+for file in $AllFiles; do
+  prefix=${file%.*}
+  if [ $output_format == "POSCAR" ]; then
+    parent=$(dirname $file)
+    name=$(basename $file)
+    prefix=${name%.*}
+    output="$parent/$output_format"_"$prefix"
+
+    echo "$file -> $output"
+    $(obabel $file -oPOSCAR -O $output 2>/dev/null)
+  else
+    echo "$file -> $prefix.$output_format"
+    $(obabel $file -O $prefix.$output_format 2>/dev/null)
+  fi
 done
--- xsdP1.perl
+++ xsdP1.perl
@@ -6,34 +6,31 @@ use Getopt::Long;
 use MaterialsScript qw(:all);
 
 # specify the directory storing the xsd files (Notice: directory format)
-my $dir="C:\\Users\\hui_zhou\\Documents\\Materials Studio Projects\\Example_Files\\Documents\\catalysts\\heterogenous\\";
-my $export_dir="C:\\Users\\hui_zhou\\Desktop\\Structures\\catalysts\\heterogenous\\";
+my $dir = "C:\\Users\\hui_zhou\\Documents\\Materials Studio Projects\\Example_Files\\Documents\\catalysts\\heterogenous\\";
+my $export_dir = "C:\\Users\\hui_zhou\\Desktop\\Structures\\catalysts\\heterogenous\\";
 
-opendir (DIR, $dir) or die "can't open the directory!";
+opendir(DIR, $dir) or die "can't open the directory!";
 my @dir = readdir DIR;
 
-foreach my $file (@dir) 
-{
-	if ($file =~ /\.xsd/)
-	{
-		print "$file \n";
-		my $prefix = $file;
-		$prefix =~ s/\.xsd//; # remove the extension of the file
-		my $doc = $Documents{"heterogenous\\$file"}; # search xsd files under heterogenous directory (Notice: directory format)
-		
-		$doc->MakeP1; # Make P1
-		
-		my $lattice=$doc->Lattice3D;
-		$lattice->Color=16724923; # set lattice `color=purple`
-		
-		my $atoms=$doc->UnitCell->Atoms;
-		$doc->CalculateBonds;
-		foreach my $atom (@$atoms)
-		{
-			$atom->Style = "Ball and stick"; # set `ball and stick` style
-		}
-  		$doc->Save;
-		$doc->Export("$export_dir\\${prefix}.msi"); # export as *.msi files
-  		$doc->Close;
-	}
+foreach my $file (@dir) {
+    if ($file =~ /\.xsd/) {
+        print "$file \n";
+        my $prefix = $file;
+        $prefix =~ s/\.xsd//;                        # remove the extension of the file
+        my $doc = $Documents{"heterogenous\\$file"}; # search xsd files under heterogenous directory (Notice: directory format)
+
+        $doc->MakeP1; # Make P1
+
+        my $lattice = $doc->Lattice3D;
+        $lattice->Color = 16724923; # set lattice `color=purple`
+
+        my $atoms = $doc->UnitCell->Atoms;
+        $doc->CalculateBonds;
+        foreach my $atom (@$atoms) {
+            $atom->Style = "Ball and stick"; # set `ball and stick` style
+        }
+        $doc->Save;
+        $doc->Export("$export_dir\\${prefix}.msi"); # export as *.msi files
+        $doc->Close;
+    }
 }
